bbdc19ce9a53b4ef52f884703758c5b104834410
Laszlo Kiss <lklaszlokiss@gmail.com>
2018-10-10 22:26:46 +0200

    tests checking the initialization of the game, 4 tests failed

--- trivia/tests/characterization/test_characterization.py
+++ trivia/tests/characterization/test_characterization.py
@@ -1,4 +1,5 @@
 import unittest
+
 from unittest.mock import  patch
 from trivia.trivia import *
 
@@ -7,6 +8,43 @@ class TestCharacterization(unittest.TestCase):
 
     def setUp(self):
         self.game = Game()
+
+
+    def test_game_initialization_no_player(self):
+        self.assertEqual(len(self.game.players), 0)
+
+    def test_game_initialization_places_are_zero(self):
+        for place in self.game.places:
+            self.assertEqual(place, 0)
+
+    def test_game_initialization_purses_are_empty(self):
+        for purse in self.game.purses:
+            self.assertEqual(purse, 0)
+
+    def test_game_initialization_nobody_in_penalty(self):
+        for person in self.game.in_penalty_box:
+            self.assertEqual(person, 0)
+
+    @unittest.skipIf(Exception, "FAILED: test_game_initialization_pop_questions_empty")
+    def test_game_initialization_pop_questions_empty(self):
+        self.assertEqual(len(self.game.pop_questions), 0)
+
+    @unittest.skipIf(Exception, "FAILED: test_game_initialization_science_questions_empty")
+    def test_game_initialization_science_questions_empty(self):
+        self.assertEqual(len(self.game.science_questions), 0)
+
+    @unittest.skipIf(Exception, "FAILED: test_game_initialization_sports_questions_empty")
+    def test_game_initialization_sports_questions_empty(self):
+        self.assertEqual(len(self.game.sports_questions), 0)
+
+    @unittest.skipIf(Exception, "FAILED: test_game_initialization_rock_questions_empty")
+    def test_game_initialization_rock_questions_empty(self):
+        self.assertEqual(len(self.game.rock_questions), 0)
+
+
+
+
+
 
 
     def tearDown(self):
